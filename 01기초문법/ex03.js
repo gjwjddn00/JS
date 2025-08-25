9826a8101a93abeca31efa3aff553cf384d6d044
gjwjddn00 <gjwjddn00@gmail.com>
2025-08-25 17:12:22 +0900

    js

---
+++ 01기초문법/ex03.js
@@ -0,0 +1,20 @@
+
+//대소문자를 전부 구분한다.
+var name = "홍길동";
+var Name = "이순신";
+
+console.log(name, Name); // ctrl + alt + n < run code
+
+// 붙어있는 단어의 첫 글자는 대문자로 사용합니다
+// 이름에 띄어쓰기는 들어갈 수 없음
+
+// var phone_number = 1234; (x)
+var phoneNumber = 1234 // (o)
+
+// 키워드는 이름로 쓸 수 없음.
+/* 
+var class = 1;
+var var = 1;
+var if = 1;
+var for = 1; 
+*/
